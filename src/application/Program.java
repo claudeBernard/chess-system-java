a14fd6c09c1de5ceb7d35b87a307d9567c87c089
claudeBernard <claude.bernard.m@hotmail.com>
2020-08-02 16:05:12 -0400

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
 
+import BoardGame.Position;
+
 public class Program {
 
     public static void main(String []args){
-        
+
+        Position test = new Position(3, 5);
+        System.out.println(test.toString());
     }
 }
